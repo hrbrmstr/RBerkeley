bd32f8c3e6693a415fd2dadc7aa6a709f91c6dc9
ahshilshah <ahshil.shah@gmail.com>
2019-03-20 15:16:03 -0700

    Updated util.R utility functions for DB_ENV

--- R/utils.R
+++ R/utils.R
@@ -15,3 +15,19 @@ is.DB <- function(x) {
 print.DB <- function(x, ...) {
   cat("<DB Handle>\n")
 }
+
+## Utility functions for DB_ENV Objects
+
+as.DB_ENV <- function(x) {
+  if(is.DB_ENV(x)) {
+    return(attr(x, "conn_id"))
+  } else stop("not a DB_ENV connection")
+}
+
+is.DB_ENV <- function(x) {
+  inherits(x, "DB_ENV") 
+}
+
+print.DB_ENV <- function(x, ...) {
+  cat("<DB_ENV Handle>\n")
+}
